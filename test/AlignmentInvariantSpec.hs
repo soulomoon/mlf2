64314053b70fbe4b52ac92f74c8b2775eaeb19ce
soulomoon <patrickwalesss@gmail.com>
2026-02-27 03:06:46 +0800

    test: add A2 invariant — witness/trace pairing

--- test/AlignmentInvariantSpec.hs
+++ test/AlignmentInvariantSpec.hs
@@ -2,8 +2,11 @@ module AlignmentInvariantSpec (spec) where
 
 import Test.Hspec
 import Control.Monad (forM_)
+import qualified Data.IntMap.Strict as IntMap
+import qualified Data.IntSet as IntSet
 import qualified Data.Set as Set
 
+import MLF.Constraint.Presolution (PresolutionResult(..))
 import MLF.Constraint.Types.Graph
     ( TyNode(..) )
 import qualified MLF.Constraint.Solved as Solved
@@ -30,3 +33,23 @@ spec = describe "Thesis alignment invariants" $ do
                             nodes = Solved.allNodes solved
                             tyExpNodes = [ n | n@TyExp{} <- nodes ]
                         tyExpNodes `shouldBe` []
+
+    describe "A2: every non-trivial edge has witness and trace" $ do
+        let corpus =
+                [ ("let-poly", ELet "id" (ELam "x" (EVar "x")) (EApp (EVar "id") (EVar "id")))
+                , ("ann-id", EAnn (ELam "x" (EVar "x")) (STForall "a" Nothing (STArrow (STVar "a") (STVar "a"))))
+                ]
+        forM_ corpus $ \(label, expr) ->
+            it ("all inst edges have witness+trace for: " ++ label) $ do
+                let result = runPipelineArtifactsDefault Set.empty expr
+                case result of
+                    Left err -> expectationFailure err
+                    Right pa -> do
+                        let pres = paPresolution pa
+                            witnesses = prEdgeWitnesses pres
+                            traces = prEdgeTraces pres
+                            witnessKeys = IntMap.keysSet witnesses
+                            traceKeys = IntMap.keysSet traces
+                        -- Every witness should have a corresponding trace
+                        let missingTraces = IntSet.difference witnessKeys traceKeys
+                        missingTraces `shouldBe` IntSet.empty
